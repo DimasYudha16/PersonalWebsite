3cb8909708b6a4e0e44395f9fc9948c519133b7d
Dimas Yudha Adi Pascayana <paskayanad@gmail.com>
2023-05-30 16:52:53 +0700

    Kurang Button Category dan CRUD nya

--- database/migrations/2023_05_01_132245_create_portofolios_table.php
+++ database/migrations/2023_05_01_132245_create_portofolios_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('portofolios', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->unsignedBigInteger('id_category')->nullable();
+            $table->foreign('id_category')
+                ->references('id')
+                ->on('categories')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
             $table->string('describe');
             $table->string('image');
             $table->timestamps();
